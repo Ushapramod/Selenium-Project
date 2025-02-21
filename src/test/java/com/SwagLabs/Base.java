310cf25158a61dd6a0bd3afffe7fe18963e08b79
Ushapramod <ushanischay2016@gmail.com>
2025-02-21 10:21:13 +0530

    pull request

--- src/test/java/com/SwagLabs/Base.java
+++ src/test/java/com/SwagLabs/Base.java
@@ -40,6 +40,7 @@ public class Base {
         //driver.get("https://www.saucedemo.com/");
         log.info("Browser launched and navigated to SwagLabs");
         System.out.println("hi hi hi hi ");
+        System.err.println("check pull request");
     }
 
     @BeforeMethod
